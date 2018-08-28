0a92216b0f3b0a7ffd4b8a4e10f4793c0da5c227
Cezar Sas <cezar.sas@gmail.com>
2018-08-28 19:31:43 +0200

    Fix

--- mpi/main.c
+++ mpi/main.c
@@ -42,7 +42,7 @@ int main(int argc, char *argv[])
 		int out = snprintf(path, 255, "/mnt/c/Users/sasce/Desktop/Matrices/matrix_%i.csv", n);
 		// printf("Loading matrix\n");
 		
-		Matrix original = read_csv(path, size);
+		Matrix original = read_csv(path, n);
 		// for(int t=0; t<10; t++){
 		{
 			Matrix matrix = duplicate_matrix(original);
